b52fee328bfc6965756269013aa2af5e92b5f1c1
Mark Wilkinson <mark@markwilkinson.me>
2016-05-18 13:43:51 +0100

    Correct Attachment Filter Name
    closes #26

--- functions/application-form.php
+++ functions/application-form.php
@@ -363,7 +363,7 @@ function wpbb_application_processing() {
 			apply_filters( 'wpbb_application_email_subject', $wpbb_mail_subject, $wpbb_application_id, $job_post ),
 			apply_filters( 'wpbb_application_email_content', $wpbb_mail_content, $wpbb_application_id, $job_post ),
 			apply_filters( 'wpbb_application_email_headers', $wpbb_email_headers, $wpbb_application_id, $job_post ),
-			apply_filters( 'wpbb_application_email_content', $wpbb_attachments, $wpbb_application_id, $job_post )
+			apply_filters( 'wpbb_application_email_attachments', $wpbb_attachments, $wpbb_application_id, $job_post )
 		);
 		
 		/* remove filter below to allow / force mail to send as html */
